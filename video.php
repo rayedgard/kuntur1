fac48bcd1b6aaeec667da9c39cc9b5880ad04b42
rayedgard <rayedgard@gmail.com>
2015-07-25 11:55:29 -0500

    inicio de proyecto

---
+++ video.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <link href="css/style.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <iframe id="video-background" width="560" height="315" src="//www.youtube.com/embed/KdEdI40iOwU?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;html5=1&amp;allowfullscreen=true&amp;wmode=transparent" frameborder="0" allowfullscreen></iframe>
+    </body>
+</html>
